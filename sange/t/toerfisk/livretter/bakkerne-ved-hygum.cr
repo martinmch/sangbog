3d24d3276541cfed778ebddf32ecefd86934e4f6
Christiansen <me@martinmch.com>
2019-12-15 00:31:20 +0100

    Add more Toerfisk

---
+++ sange/t/toerfisk/livretter/bakkerne-ved-hygum.cr
@@ -0,0 +1,49 @@
+{title: Bakkerne i Hygum}
+{artist: Tørfisk}
+{album: Livretter}
+
+Jeg er barnefoedt derhjemme
+som de fleste born jo er
+og livet er jo som om det er ikke saerligt saer
+de fleste i familien
+de hader mig saa kaert
+charme havde jeg ogsaa
+dengang jeg var lille
+
+{soc}
+i bakkerne ved hygum
+der sad min pige og jeg
+verden den stod stille da
+hun kiggede paa mig
+forlad mig aldrig sagde hun
+selfolgelig sagde jeg nej
+der var dejligt der i bakkerne ved hygum
+{eoc}
+
+Dengagn jeg kom i skole var jeg kun en lille flos
+men hurtig fik jeg bejlet til skolens bedste tos
+men ska jeg vaere aerlig saa var jeg lidt nervoes
+da vi sad paa cyklerne paa vej til hygum.
+
+Og da jeg saa blev aeldre, da sagde mi nfar til mig
+pas paa med piger, for du ka saa nemt forelske dig
+når faelden foerst er klappet, ka du ikke sige nej
+som man siger, saa er dine dage talte.
+
+{chorus}
+
+Så tog jeg ud og sejle som mangen somand gor
+ja for at se paa verden, det synes jeg man boer
+det kraever jo sin mand ja for at turde toer
+og i tankerne der er man ofte hjemme
+
+{chorus}
+
+Men nu er den tid ovre, det kan i lige tro
+med kone boern derhjemme, der har jeg fundet ro
+hvis bornene er ude, saa er vi kun os to,
+ja, saa ved i alle hvor i os kan finde.
+
+{chorus}
+
+{chorus}
